e787ba2644fe700d72b3029a17d3159521e2169d
MrVictor42 <guin-hu@hotmail.com>
2022-05-13 17:41:49 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './template/navbar/navbar.component';
 import { SidebarComponent } from './template/sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
+import { ClientsFormComponent } from './components/clients/clients-form/clients-form.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    HomeComponent
+		NavbarComponent,
+		SidebarComponent,
+		HomeComponent,
+  ClientsFormComponent
 	],
 	imports: [
 		BrowserModule,
